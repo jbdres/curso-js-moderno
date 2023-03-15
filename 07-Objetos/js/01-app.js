c50f26144d7d3d50aa90efc73db7afda42badd0e
Julian Bermudez <jbetov@jbetov.com>
2023-03-14 19:23:23 -0500

    Create an Object in JavaScript

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,15 @@
+/* Crear Objetos en JavaScript * */
+const nombre = "Monitor de 20 pulgadas";
+const precio = 300;
+const disponible = true;
+
+// Un objeto agrupa todos los datos en una sola variable
+
+// Object literal 
+const producto = { // Separamos los atributos con comas
+    nombre: "Monitor de 20 pulgadas", // llave y valor
+    precio: 300,
+    disponible: true
+}; 
+
+console.log(producto);
