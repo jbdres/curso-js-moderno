c00a1732b0487c26253f12b82d0552cd5a5e2fbe
Julian Bermudez <jbetov@jbetov.com>
2023-07-07 12:37:23 -0500

    Create an Array

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,11 @@
+/** Crear Arrays en JavaScript */
+// Sirve para agrupar elementos del mismo tipo
+const numeros = [10, 20, 30];
+console.log(numeros);
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+console.log(meses);
+
+// Arreglo que contiene datos de todo tipo
+const deTodoTipo = ['Hola', 12, 52, null, true, {nombre:'Julian', trabajo:'Programador'}, [1,2,3]];
+console.log(deTodoTipo);
