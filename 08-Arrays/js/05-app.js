df6a75813d4ae66aa3ac1bdb0bebc6e9bf4ee97f
Julian Bermudez <jbetov@jbetov.com>
2023-07-20 10:13:16 -0500

    Appending new elements to the end or beginning of an array

---
+++ 08-Arrays/js/05-app.js
@@ -0,0 +1,11 @@
+/** Agregar nuevos elementos al final o inicio de un array */
+const meses = ['Enero', 'Febrero', 'Marzo'];
+console.log(meses);
+
+// Agregar al inicio del array
+meses.unshift('Abril');
+console.log(meses);
+
+// Agregar al final del array
+meses.push('Mayo');
+console.log(meses);
