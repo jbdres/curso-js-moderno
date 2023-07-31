311b319b4ae0fa47f0f02e8459b8758faa239a6a
Julian Bermudez <jbetov@jbetov.com>
2023-07-31 18:43:27 -0500

    Function Declaration

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,17 @@
+// Declaración de Funcion ( Function Declaration )
+
+function sumar() { // Parametros de la función
+    // Cuerpo de la función
+    console.log(2+2);
+}
+
+// Llamando a la función
+sumar();
+
+
+// Expresión de Función ( Function Expression )
+const sumar2 = function (){
+    console.log(3+3);
+}
+
+sumar2();
