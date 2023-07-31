dde2c43617abef3d8ee08e7e154559ee50d4681f
Julian Bermudez <jbetov@jbetov.com>
2023-07-31 18:54:27 -0500

    Parameters and Arguments in Functions

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,11 @@
+/** Parametros y Argumentos en Funciones */
+function sumar(a, b) { // a y b son parametros
+    console.log(a+b);
+}
+
+sumar(2,3); // 2 y 3 son argumentos
+
+/** al hacer uso de parametros y argumentos la función sirve para cualquier valor que se ingrese */
+sumar(4,9);
+sumar(1,8);
+sumar(5,1);
