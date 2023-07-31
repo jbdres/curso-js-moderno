018d182e059d4876a02df79854af26f042b31956
Julian Bermudez <jbetov@jbetov.com>
2023-07-31 18:50:05 -0500

    The Difference between Function and Method

---
+++ 09-Funciones/js/04-app.js
@@ -0,0 +1,11 @@
+/** La Diferencia entre Función y Método */
+// Nota: Basicamente la función y el método son practicamente la misma cosa ya que ambas son reutilizables
+
+const numero1 = 20;
+const numero2 = "20";
+
+// Esta es una función. Estructura: nombre + parentesis
+console.log(parseInt(numero2));
+
+// Este es un método. Estructura: objeto + . + nombre + parentesis
+console.log(numero1.toString());
