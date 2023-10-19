d5f565d540259315332c4d6b4e9646f79f0cf55f
jbdres <jbdres@jbdres.com>
2023-10-19 18:11:27 -0500

    Comparing less than and greater than

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,16 @@
+/** Comparando Mayor o Menor que */
+const dinero = 500;
+const totalAPagar = 300;
+
+if (dinero > totalAPagar) {
+    console.log("Si podemos pagar");
+} else {
+    console.log("Fondos insuficientes");
+}
+
+/**
+ * * < Menor
+ * * > Mayor
+ * * <= Menor que
+ * * >= Mayot que
+ */
