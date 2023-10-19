0c4451255a0bf82becd814a318582f96c7193ba8
jbdres <jbdres@jbdres.com>
2023-10-19 17:45:24 -0500

    Creating an if statement

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,9 @@
+/** Nuevo capitulo: Estructuras de control */
+// Creando un if
+
+let miVariable = false;
+if (miVariable == true) {
+    console.log("Entra en el if");
+} else {
+    console.log("No entra en el if");
+}
