cd5853233b57d25ca38421087d4ed8588f7bf5b0
jbdres <julian.betov@gmail.com>
2024-04-30 10:12:57 -0500

    Export and import a class

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,8 +1,12 @@
-// Exportar e importar funciones
-import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo } from "./cliente.js";
+// Exportar e importar una clase
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente.js";
     // Variables
 console.log(nombreCliente);
 console.log(ahorro);
     // Funciones
 console.log(mostrarInformacion(nombreCliente, ahorro));
 tieneSaldo(ahorro);
+    // Clases
+const cliente = new Cliente(nombreCliente, ahorro);
+console.log(cliente);
+console.log(cliente.mostrarInformacion());
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -1,4 +1,4 @@
-// Exportar e importar funciones
+// Exportar e importar una clase
     // Variables
 export const nombreCliente = 'Julian';
 export const ahorro = 200;
@@ -12,5 +12,15 @@ export function tieneSaldo(ahorro) {
         console.log('Si tiene saldo');
     } else {
         console.log('No tiene saldo');
+    }
+}
+    // Clases
+export class Cliente {
+    constructor(nombre, ahorro) {
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+    mostrarInformacion() {
+        return `Soy ${this.nombre} y tengo $${this.ahorro} dolares`;
     }
 }
