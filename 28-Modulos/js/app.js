94294860b554677308f8c462acb4b5223e9c88f4
jbdres <julian.betov@gmail.com>
2024-04-30 10:05:07 -0500

    Export and import functions

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,15 +1,8 @@
-// Basicos de los modulos en ES6
-/* ------ Llamar a una variable en otro archivo js, este archivo se llama desde el mismo HTML y no es type="module". Es la forma en la que se ha hecho hasta el momento, sirve de ejemplo ------ */
-
-// console.log(nombreUsuario);
-    // Ya que 'nombreUsuario' en cliente.js se define en una funcion IIFE, solo se tiene acceso local a la variable, osea en el archivo en el que se define... por lo cual se imprime 'Andres' y no 'Julian', gracias a que 'Andres' esta en la variable global.
-
-/* ------ Importar variables desde otro archivo ------
-   - Para que esto funcione, cuando se vincule este archivo js al HTML, tiene que ser de tipo 'module', asi: <script src="js/app.js" type="module"></script> 
-*/
-
-import { nombreCliente } from "./cliente.js";
-import { ahorro } from "./cliente.js";
-
+// Exportar e importar funciones
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo } from "./cliente.js";
+    // Variables
 console.log(nombreCliente);
 console.log(ahorro);
+    // Funciones
+console.log(mostrarInformacion(nombreCliente, ahorro));
+tieneSaldo(ahorro);
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -1,22 +1,16 @@
-// Basicos de los modulos en ES6
-/**
- * Utilizar elementos de este archivo en otros.
- * - Facilmente nosostros podriamos llamar a cada uno de los archivos javascript desde el mismo HTML, cada archivo tendria acceso a la informacion del otro. Cuando se trabaja con varias personas, esta metodologia puede tener varios inconvenientes como variables con el mismo nombre pero definidas en archivos distintos, etc.
- * - Existen formas para utilizar datos de otro archivo .js, como: funciones IIFE y export.
- */
-
-/** ----------------------------------------------
-// IIFE: Poner el codigo en una funcion que se ejecuta inmediatamene,a las variables aqui definidas se tiene solo acceso desde el mismo archivo, a menos que se pongan en la ventana global.
-* ! Para probar esta funcion hay que descomentarla y llamarla desde el HTML y comentar el bloque de codigo de 'export' e 'import' en el archivo 'apps.js' para este ultimo. Tambien se descomenta la salida a consola en la linea 3 de 'app.js'.
-*/
- 
-/* (function() {
-    console.table('Desde un IIFE');
-    const nombreUsuario = 'Carlos'; // Acceso local a la variable
-    window.nombreUsuario = 'Rodriguez'; // Acceso global a la variable
-        // Desventaja: La ventana global se estaria llenando de codigo innecesario
-})(); // Estos ultimos parentesis son los que ejecutan la funcion inmediatamente */
-
-//-------------------- export --------------------
+// Exportar e importar funciones
+    // Variables
 export const nombreCliente = 'Julian';
 export const ahorro = 200;
+    // Funciones
+export function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
+}
+
+export function tieneSaldo(ahorro) {
+    if (ahorro > 0) {
+        console.log('Si tiene saldo');
+    } else {
+        console.log('No tiene saldo');
+    }
+}
