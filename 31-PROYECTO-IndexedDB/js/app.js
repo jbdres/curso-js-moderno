6343a24bcd5e218eb2e3b83bda1b0457c2bb757d
jbdres <julian.betov@gmail.com>
2024-06-04 19:28:55 -0500

    Creating Database

--- 31-PROYECTO-IndexedDB/js/app.js
+++ 31-PROYECTO-IndexedDB/js/app.js
@@ -1,4 +1,5 @@
-
+// Crear la base de datos para el proyecto de citas en indexedDB
+let DB;
 const mascotaInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
 const telefonoInput = document.querySelector('#telefono');
@@ -18,6 +19,11 @@ const heading = document.querySelector('#administra');
 
 
 let editando = false;
+
+window.onload = () => {
+    eventListeners();
+    createDB();
+}
 
 
 // Eventos
@@ -262,4 +268,39 @@ function cargarEdicion(cita) {
 
     editando = true;
 
+}
+
+function createDB() {
+
+    // Crear la base de datos en version 1.0
+    const crearDB = window.indexedDB.open('citas', 1);
+    // Si hay un error
+    crearDB.onerror = function() {
+        console.log('Hubo un error');
+    }
+    // Si todo sale bien
+    crearDB.onsuccess = function() {
+        DB = crearDB.result;
+    }
+
+    // Definir el schema
+    crearDB.onupgradeneeded = function(e) {
+
+        const db = e.target.result;
+        const objectStore = db.createObjectStore('citas', {
+            keyPath: 'id',
+            autoIncrement: true
+        });
+        // Definir todas las columnas
+        objectStore.createIndex('mascota', 'mascota', { unique:false });
+        objectStore.createIndex('propietario', 'propietario', { unique:false });
+        objectStore.createIndex('telefono', 'telefono', { unique:false });
+        objectStore.createIndex('fecha', 'fecha', { unique:false });
+        objectStore.createIndex('hora', 'hora', { unique:false });
+        objectStore.createIndex('sintomas', 'sintomas', { unique:false });
+        objectStore.createIndex('id', 'id', { unique:false });
+
+        console.log('DB creada y lista');
+    }
+    
 }
