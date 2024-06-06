a595d6943534db9ae82f3c80c03ea0e449f49260
jbdres <julian.betov@gmail.com>
2024-06-06 13:00:17 -0500

    Adding new clients to the database

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -37,6 +37,30 @@
             return;
         }
 
+        // Crear un objeto con la informacion 
+            // Contrario al destructuing
+        const cliente = {id: Date.now(), nombre, email, telefono, empresa};
+
+        crearNuevoCliente(cliente);
+
+    }
+
+    function crearNuevoCliente(clientObj) {
+
+        // Agregar el objeto a la base de datos
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.add(clientObj);
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+        transaction.oncomplete = function() {
+            imprimirAlerta('Cliente agregado correctamente');
+            setTimeout(() => {
+                window.location.href = './index.html'
+            }, 2000);
+        }
 
     }
 
