217e81ab5e2771c1c0851a88c6cb2fbd2ec2cc94
jbdres <julian.betov@gmail.com>
2024-06-07 20:26:26 -0500

    Deleting a client form the database

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -2,6 +2,7 @@
 (function() {
 
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -12,6 +13,32 @@
         }
 
     });
+
+    listadoClientes.addEventListener('click', eliminarCliente);
+
+    function eliminarCliente(e) {
+        e.preventDefault();
+        // console.log(e.target); // Obtener el elemento en concreto al que se le ha dado click
+        if (e.target.classList.contains('eliminar')) {
+            // Obtener el id del cliente a eliminar
+            const idEliminar = Number(e.target.dataset.cliente);
+            const confirmar = confirm('Desea eliminar este cliente?');
+            if (confirmar) {
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+                objectStore.delete(idEliminar);
+                transaction.onerror = function() {
+                    console.log('Hubo un error');
+                }
+                transaction.oncomplete = function () {
+                    console.log('Cliente eliminado correctamente');
+                    // Eliminar el registro en el listado de clientes
+                    e.target.parentElement.parentElement.remove();
+                }
+            }
+        }
+
+    }
 
     // Crea la base de datos de indexedDB
     function crearDB() {
@@ -59,7 +86,6 @@
 
                     const {id,nombre, empresa, email, telefono} = cursor.value;
 
-                    const listadoClientes = document.querySelector('#listado-clientes');
                     listadoClientes.innerHTML += `
                     <tr>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -74,14 +100,12 @@
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                             <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                         </td>
                     </tr>
                     `;
                     cursor.continue();
                     
-                } else {
-                    console.log('No hay mas registros');
                 }
             }
 
